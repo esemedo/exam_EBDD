a9610d387ae4f7a07dc659241c9ad8fdcbe57bf5
Elisa Semedo <elisasemedo07@gmail.com>
2025-01-19 15:48:22 +0100

    Mise en place clé d'API

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require("cors")
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yaml');
 const fs = require("node:fs");
+const { checkApiKey } = require("./middleware/checkApiKey");
 const swaggerFile = fs.readFileSync('./swagger/swagger.yaml', 'utf8'); 
 const swaggerDocument = YAML.parse(swaggerFile);
 
@@ -16,11 +17,11 @@ app.use(cors({
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json())
-app.use("/api/products", require("./routes/products"))
-app.use("/api/categories", require("./routes/categories"))
-app.use("/api/providers", require("./routes/providers"))
-app.use("/api/orders", require("./routes/orders"))
-app.use("/api/clients", require("./routes/clients"))
+app.use("/api/products", checkApiKey, require("./routes/products"))
+app.use("/api/categories", checkApiKey, require("./routes/categories"))
+app.use("/api/providers",checkApiKey, require("./routes/providers"))
+app.use("/api/orders",checkApiKey, require("./routes/orders"))
+app.use("/api/clients", checkApiKey, require("./routes/clients"))
 app.listen(3001, ()=>{
     console.log("Listen on port 3001");
     }
